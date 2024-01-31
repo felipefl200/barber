190ab44f60bae7f422b5fa4198a769c43ad4a22d
felipefl200 <felipefl200@gmail.com>
2024-01-31 14:41:23 -0300

    feat: add nextauth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+
+const handler = NextAuth({
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        })
+    ]
+})
