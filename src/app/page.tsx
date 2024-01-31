bda887549dfeba85f8a134dfe6d7238f5f66d19d
felipefl200 <felipefl200@gmail.com>
2024-01-31 16:12:48 -0300

    fix: change name file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/Header'
+import { Header } from '@/components/header'
 import { BarbershopItem } from '@/components/barbershop-item'
 import { BookingItem } from '@/components/booking-item'
 import { Search } from '@/components/search'
