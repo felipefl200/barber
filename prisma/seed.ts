d5f539ea9cf93192c4690ec45b89e14cd24d267a
felipefl200 <felipefl200@gmail.com>
2024-01-31 10:37:14 -0300

    fix DB schema and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -117,6 +117,7 @@ async function seedDatabase() {
             name: service.name,
             description: service.description,
             price: service.price,
+            imageUrl: service.imageUrl,
             barbershop: {
               connect: {
                 id: barbershop.id,
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 import { Copyright } from 'lucide-react'
 export function Footer() {
     return (
-        <div className="fixed bottom-0 w-full bg-secondary px-5 py-6 opacity-75">
+        <div className="absolute bottom-0 w-full bg-secondary px-5 py-6 opacity-75">
             <p className="text-xs-font-bold flex items-center justify-center gap-2 text-gray-400">
                 <Copyright /> 2023 Copyright FFL Barber
             </p>
