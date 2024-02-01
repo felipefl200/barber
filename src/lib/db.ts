4fe8d7eea4c3ad7cb9ec3fe8729d9d05042a98cf
felipefl200 <felipefl200@gmail.com>
2024-02-01 14:05:21 -0300

    feat: log prisma dev mode

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  if (process.env.NODE_ENV === 'production') return new PrismaClient()
+  
+  return new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
